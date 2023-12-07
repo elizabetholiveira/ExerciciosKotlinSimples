a8fabb78e5b90260440a879dbb0b65e8ddbe8e56
Elizabeth Oliveira <elizabeth.oliveira@zup.com.br>
2023-12-07 11:32:30 -0300

    commit main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+import kotlin.NumberFormatException
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var entradaCorreta = false
+
+    do {
+        println("Selecione qual exercício deseja ver:")
+
+        var i = 1
+
+        //TODO Ajeitar que isso tá fazendo a lista de exercícios rodar todinha
+        val (listaExercicios, qtdExer) = exer()
+
+        while (i <= qtdExer) {
+            println("$i ) Exercício $i")
+            i++
+        }
+
+        val input = readln()
+
+        try {
+            val entradaDoUsuario: Int = input.toInt() ?: throw NumberFormatException("Entrada inválida")
+
+            entradaCorreta = true
+            if (entradaDoUsuario <= qtdExer) {
+                when (entradaDoUsuario) {
+                    entradaDoUsuario -> listaExercicios[entradaDoUsuario - 1]
+                }
+            }
+                else {
+                    println("Número inválido")
+                    entradaCorreta = false
+                }
+        } catch (e: NumberFormatException) {
+            println("Entrada inválida. Por favor, digite apenas o número")
+        }
+    } while (!entradaCorreta)
+
+}
+
+fun exer(): Pair<List<Unit>, Int> {
+    var exercicios = listOf(
+        exer01.main()
+    )
+
+    return exercicios to exercicios.size
+}
